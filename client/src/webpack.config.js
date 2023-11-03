4fa08c1414b473f591c93abdada6e8f45ffe0a72
Chris Chang Change <changc21@gmatc.matc.edu>
2023-11-03 01:00:26 -0500

    Webpage is not compiling error

---
+++ client/src/webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    resolve : {
+        fallback: { "zlib": require.resolve("browserify-zlib") }
+    }
+}
+module.exports = {
+    resolve : {
+        fallback: { "querystring": require.resolve("querystring-es3") }
+    }
+}
+module.exports = {
+    resolve : {
+        fallback: { "path": require.resolve("path-browserify") }
+    }
+}
+module.exports = {
+    resolve : {
+        fallback: { "crypto": require.resolve("crypto-browserify") }
+    }
+}
+module.exports = {
+    resolve : {
+        fallback:  { "stream": require.resolve("stream-browserify") }
+    }
+}
+module.exports = {
+    resolve : {
+        fallback:  { "http": require.resolve("stream-http") }
+    }
+}
+module.exports = {
+    resolve : {
+        fallback:  { "url": require.resolve("url") }
+    }
+}
+module.exports = {
+    resolve : {
+        fallback:  { "http": require.resolve("stream-http") }
+    }
+}
